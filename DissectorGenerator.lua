2b8630833acc8c85f73d93a3612e02a3663cbc7c
HarryLeSanto <harry.lesanto10@hotmail.co.uk>
2025-04-04 23:21:30 +0100

    Added file generation.

--- DissectorGenerator.lua
+++ DissectorGenerator.lua
@@ -1,12 +1,29 @@
+local data_field = Field.new("data")
+
 local function NewDissector()
-    local function DissectorMenu(name, description, protocol)
+	local message = "Enter payload here.";
+
+    local function DissectorMenu(name, protocol, port)
         local window = TextWindow.new("New Dissector - " .. name);
-        local message = "Enter payload here.";
+
+		local function GenerateDissector()
+			local filename = protocol .. "_dissector.lua"
+			local path = ("C:\\Users\\Harry\\AppData\\Roaming\\Wireshark\\plugins") .. "\\" .. filename
+			local file = io.open(path, "w")
+			file:write(window:get_text())
+			file:close()
+		end
+
+
         window:set(message);
 		window:set_editable(true);
+
+		window:add_button("Generate", GenerateDissector)
+		window:add_button("Clear", function() window:clear() end)
+		window:add_button("Cancel", function() window:close() end)
     end
 
-    new_dialog("New Dissector", DissectorMenu, "Name", "Description", "Protocol")
+    new_dialog("New Dissector", DissectorMenu, "Name", "Protocol", "Port")
 end
 
 -- Create the menu entry
